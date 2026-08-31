f42dba096392fe220320104a072cc46ea91d2964
divyanshichugh4 <divyanshi4456@gmail.com>
2026-08-31 14:38:31 +0530

    Add AWS S3 bucket resource to Terraform config
    Add S3 bucket resource configuration to main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket = "t-aws-lab-divyanshi-2026-unique"
+
+  tags = {
+    Name        = "T-AWS-Lab-Bucket"
+    Environment = "Lab"
+  }
 }
