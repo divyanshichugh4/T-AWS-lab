e1d0df070b0f871e5a30b2dcecfaae5b3f719b4f
divyanshichugh4 <divyanshi4456@gmail.com>
2026-09-14 13:09:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,23 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+# Main S3 bucket
 resource "aws_s3_bucket" "lab_bucket" {
   bucket = "t-aws-lab-divyanshi-2026"
 }
 
+# Separate bucket for S3 access logs
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "t-aws-logs-divyanshi-2026"
 }
 
+# Allow S3 logging service to write logs
 resource "aws_s3_bucket_policy" "log_bucket_policy" {
   bucket = aws_s3_bucket.log_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
         Sid    = "S3ServerAccessLogsPolicy"
@@ -40,6 +44,7 @@ resource "aws_s3_bucket_policy" "log_bucket_policy" {
   })
 }
 
+# Enable access logging for the main S3 bucket
 resource "aws_s3_bucket_logging" "lab_bucket_logging" {
   bucket = aws_s3_bucket.lab_bucket.id
 
@@ -47,11 +52,13 @@ resource "aws_s3_bucket_logging" "lab_bucket_logging" {
   target_prefix = "log/"
 }
 
+# Enforce HTTPS-only access
 resource "aws_s3_bucket_policy" "lab_bucket_policy" {
   bucket = aws_s3_bucket.lab_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
         Sid       = "DenyInsecureTransport"
