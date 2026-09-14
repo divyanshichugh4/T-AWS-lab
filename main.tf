3d7bb1df0186a90c9477c183aaa76e73d0165c5d
divyanshichugh4 <divyanshi4456@gmail.com>
2026-09-14 13:01:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,37 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+    required_providers {
+        ...
     }
-  }
 }
 
 provider "aws" {
-  region = "eu-north-1"
+    ...
 }
 
-resource "aws_s3_bucket" "lab_bucket" {
-  bucket = "t-aws-lab-divyanshi-2026"
+resource "aws_s3_bucket" "YOUR_MAIN_BUCKET_NAME" {
+    ...
+}
+
+HTTPS BUCKET POLICY
+↓
+resource "aws_s3_bucket_policy" "..." {
+    ...
+}
+
+LOGGING BUCKET
+↓
+resource "aws_s3_bucket" "log_bucket" {
+    ...
+}
+
+LOGGING BUCKET POLICY
+↓
+resource "aws_s3_bucket_policy" "log_bucket_policy" {
+    ...
+}
+
+S3 LOGGING
+↓
+resource "aws_s3_bucket_logging" "lab_bucket_logging" {
+    ...
 }
