5a22c450d3d8193e9181ba2d3b13423cf0ae24a0
divyanshichugh4 <divyanshi4456@gmail.com>
2026-09-14 13:19:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,49 +26,56 @@ resource "aws_s3_bucket_logging" "lab_bucket_logging" {
 }
 
 resource "aws_s3_bucket_policy" "lab_bucket_policy" {
-  bucket = aws_s3_bucket.lab_bucket.id
+  bucket = "t-aws-lab-divyanshi-2026"
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "DenyInsecureTransport",
-      "Effect": "Deny",
-      "Principal": "*",
-      "Action": "s3:*",
-      "Resource": [
-        "${aws_s3_bucket.lab_bucket.arn}",
-        "${aws_s3_bucket.lab_bucket.arn}/*"
-      ],
-      "Condition": {
-        "Bool": {
-          "aws:SecureTransport": "false"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "LabBucketPolicy"
+
+    Statement = [
+      {
+        Sid    = "HTTPSOnly"
+        Effect = "Deny"
+
+        Principal = {
+          "AWS" = "*"
+        }
+
+        Action = "s3:*"
+
+        Resource = [
+          aws_s3_bucket.lab_bucket.arn,
+          "${aws_s3_bucket.lab_bucket.arn}/*"
+        ]
+
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
         }
       }
-    }
-  ]
-}
-POLICY
+    ]
+  })
 }
 
 resource "aws_s3_bucket_policy" "log_bucket_policy" {
   bucket = aws_s3_bucket.log_bucket.id
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "S3ServerAccessLogsPolicy",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "logging.s3.amazonaws.com"
-      },
-      "Action": "s3:PutObject",
-      "Resource": "${aws_s3_bucket.log_bucket.arn}/*"
-    }
-  ]
-}
-POLICY
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "S3ServerAccessLogsPolicy"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "logging.s3.amazonaws.com"
+        }
+
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.log_bucket.arn}/*"
+      }
+    ]
+  })
 }
